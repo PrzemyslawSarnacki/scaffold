72f146ac8f09cb6fac3df88e0cc265ab91167a46
Przemysław Sarnacki <przemos100@gmail.com>
2022-02-08 22:58:44 +0100

    feat: add fiber template

--- scaffold/template_engine.go
+++ scaffold/template_engine.go
@@ -1,6 +1,7 @@
 package scaffold
 
 import (
+	"io"
 	"os"
 	"path/filepath"
 	"strings"
@@ -36,6 +37,36 @@ func (templEngine *templateEngine) visit(path string, f os.FileInfo, err error) 
 
 		templEngine.Templates = append(templEngine.Templates, templ)
 
+	} else if ext := filepath.Ext(path); ext == ".png" || ext == ".gif" {
+		src, err := os.Open(path)
+		if err != nil {
+			return pkgErr.WithStack(err)
+		}
+		defer src.Close()
+
+		basepath := templEngine.basePath
+		distRelFilePath, err := filepath.Rel(basepath, filepath.Join(filepath.Dir(path), filepath.Base(path)))
+		if err != nil {
+			return pkgErr.WithStack(err)
+		}
+
+		distAbsFilePath := filepath.Join(templEngine.currDir, distRelFilePath)
+
+		if err := os.MkdirAll(filepath.Dir(distAbsFilePath), os.ModePerm); err != nil {
+			return pkgErr.WithStack(err)
+		}
+
+		dist, err := os.Create(distAbsFilePath)
+		if err != nil {
+			return pkgErr.WithStack(err)
+		}
+		defer dist.Close()
+
+		if _, err := io.Copy(dist, src); err != nil {
+			return pkgErr.WithStack(err)
+		}
+
+		// log.Printf("Create %s \n", distRelFilePath)
 	} else if mode := f.Mode(); mode.IsRegular() {
 		templateFileName := filepath.Base(path)
 
